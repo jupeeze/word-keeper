fcf86c51fc99ad903615ed11f47ede1c2148ea2d
jupeeze <pokegames1578@gmail.com>
2025-11-28 11:35:34 +0900

    Update songIndex.ts

--- src/data/songIndex.ts
+++ src/data/songIndex.ts
@@ -2,21 +2,18 @@
 import elevenData from './songs/eleven-ive.json';
 import fiestaData from './songs/fiesta-iz*one.json';
 import whatMakesYouBeautifulData from './songs/what-makes-you-beautiful_one-direction.json';
-import twinkletwinkleData from './songs/twinkle-twinkle-korean.json';
 import type { Song } from '@/types';
 
 // Type assertions for JSON imports
 const eleven = elevenData as Song;
 const fiesta = fiestaData as Song;
 const whatMakesYouBeautiful = whatMakesYouBeautifulData as Song;
-const twinkletwinkle = twinkletwinkleData as Song;
 
 // Export as array
 export const songs: Song[] = [
     eleven,
     fiesta,
     whatMakesYouBeautiful,
-    twinkletwinkle,
 ];
 
 export default songs;
